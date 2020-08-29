3f22fefde6c770b0fe2a1163e27b86955d8255e0
Jominseo <minseocho0309@gmail.com>
2020-08-29 20:25:59 +0900

    callback commit

--- todolist/lib/widgets/tasks_list.dart
+++ todolist/lib/widgets/tasks_list.dart
@@ -3,32 +3,32 @@ import 'package:todolist/models/task.dart';
 import 'package:todolist/widgets/tasks_tile.dart';
 
 class TasksList extends StatefulWidget {
+
+  final List<Task> tasks;
+
+  TasksList(this.tasks);
+
   @override
   _TasksListState createState() => _TasksListState();
 }
 
 class _TasksListState extends State<TasksList> {
-  List<Task> tasks = [
-    Task(name: 'Buy milk'),
-    Task(name: 'Buy eggs'),
-    Task(name: 'Buy bread'),
-  ];
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
         return TasksTile(
-          taskTitle: tasks[index].name,
-          isChecked: tasks[index].isDone,
+          taskTitle: widget.tasks[index].name,
+          isChecked: widget.tasks[index].isDone,
           checkboxCallback: (checkboxState) {
             setState(() {
-              tasks[index].toggleDone();
+              widget.tasks[index].toggleDone();
             });
           },
         );
       },
-      itemCount: tasks.length,
+      itemCount: widget.tasks.length,
     );
   }
 }
